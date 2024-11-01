fa665c6feeff1ad857e76b53d5aef9d6564cdede
luqman.rahardjo@gmail.com <luqman.rahardjo@gmail.com>
2024-11-01 09:01:18 +0700

    removed

--- gui/config.py
+++ gui/config.py
@@ -1,5 +1,5 @@
-MQTT_SERVER = "mqtt.luqmanr.xyz"
+MQTT_SERVER = "0.0.0.0"
 MQTT_PORT = 1883
 MQTT_CLIENTID_PREFIX = "MSI-LUQ-"
-MQTT_USER = "luqmanr"
-MQTT_PASSWORD = "9194"
+MQTT_USER = "iotsbm"
+MQTT_PASSWORD = "belajariot"
